c667522aaea7972bdc4a3bac8f9627bb1c48b2d5
JudsonSilva <39493081+JudsonSilva@users.noreply.github.com>
2018-05-21 12:41:11 -0300

    Create Etapa1.js

---
+++ Etapa1.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  fill(255,20,45);
+  rect(250, 300, 75, 500);
+
+  fill(20, 200, 200);
+  ellipse(50,300, 55,100);
+  fill(20, 20, 200);
+  ellipse(70,360, 15,55);
+    fill(20, 20, 200);
+  ellipse(30,360, 15,55);
+    fill(20, 20, 200);
+  ellipse(90,280, 55,15);
+      fill(20, 20, 200);
+  ellipse(30,290, 15,55);
+        fill(20, 20, 200);
+  ellipse(50,240, 55,55);
+}
